6cdd5f0f6e4054bf52845d648ada1b7a9cd1f976
AnxheloZuna <anxhelozuna@yahoo.com>
2022-06-06 14:08:07 +0200

    Initial commit

---
+++ src/FirstJavaProgram.java
@@ -0,0 +1,9 @@
+public class FirstJavaProgram {
+    public static void main(String[] args) {
+
+        System.out.println("My name is Coding Dojo\n");
+        System.out.println("I am 100 years old\n");
+        System.out.println("My hometown is Burbank, Ca\n");
+
+    }
+}
